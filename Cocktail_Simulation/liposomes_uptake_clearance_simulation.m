e37ccc29ba1c0ee4f7d0ca76e07c374e4d4abb8b
mihaliskavousanakis <86114672+mihaliskavousanakis@users.noreply.github.com>
2024-01-19 15:47:28 +0200

    Add files via upload

--- Cocktail_Simulation/liposomes_uptake_clearance_simulation.m
+++ Cocktail_Simulation/liposomes_uptake_clearance_simulation.m
@@ -12,10 +12,12 @@ end
 x(1)=1.5e-13/(300e-6)^2*3600;  % effective diffusivity of liposomes
 x(2)=1.9e-9/(300e-6)*3600; % mass transfer coefficient during uptake
 x(3)=5.8e-9/(300e-6)*3600; % mass transfer coefficient during clearance
-Dd = 8e-4/(300e-6)^2*3600; % effective drug diffusivity 
+Dd =2.e-11/(300e-6)^2*3600; % effective drug diffusivity 
+hdup=1.7e-8/(300e-6)*3600; % drug mass transfer coefficient during uptake
+hdcl=2.9e-8/(300e-6)*3600; % drug mass transfer coefficient during clearance
 
 % uptake simulation
-[xpt,ytheor_up]=specliposome_uptake([x(1) x(2) Dd]);
+[xpt,ytheor_up]=specliposome_uptake([x(1) x(2) Dd hdup]);
 Ytheorup=[];
 for met=1:13
     Ytheorup(end+1,:)=ytheor_up(met,:); 
@@ -24,7 +26,7 @@ end
 % Clearance simulation
 y0=ytheor_up(end,:); y0=y0';
 tspan1=[0:0.5:8];
-[xpt,ytheor_cl]=specliposome_clearance([x(1) x(3) Dd],y0,tspan1);
+[xpt,ytheor_cl]=specliposome_clearance([x(1) x(3) Dd hdcl],y0,tspan1);
 Ytheorcl=[];
 for met=1:length(tspan1)
     Ytheorcl(end+1,:)=ytheor_cl(met,:); 
