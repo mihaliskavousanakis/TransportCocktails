6dd8145e0f2ac8b4d351aecbb05a919a4245102b
mihaliskavousanakis <86114672+mihaliskavousanakis@users.noreply.github.com>
2024-01-19 15:41:19 +0200

    Add files via upload

--- Cocktail_Simulation/antibodies_uptake_clearance_simulation.m
+++ Cocktail_Simulation/antibodies_uptake_clearance_simulation.m
@@ -12,9 +12,9 @@ end
 Deff=6e-12/(300e-6)^2*3600; % effective diffusivity of Ab in the spheroids
 hup=2.5e-10/(300e-6)*3600; % mass transfer coefficient during uptake
 hcl=8e-7/(300e-6)*3600; % mass transfer coefficient during clearance
-koff=1e-2*3600; % dissociation rate constant
-KD=4.5; % dissociation equilbrium constant
-kr=5.5e-6*3600; % internalization rate constant
+koff=4e-3*3600; % dissociation rate constant  
+KD=5; % dissociation equilbrium constant 
+kr=1.4e-5*3600; % internalization rate constant   
 
 
 % Uptake experiment
